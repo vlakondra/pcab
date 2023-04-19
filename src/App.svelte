e3320ba8632ae63f053eba0931e61600f2270717
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-04-19 16:01:38 +0500

    login form

--- src/App.svelte
+++ src/App.svelte
@@ -10,14 +10,14 @@
     NavHamburger,
   } from "flowbite-svelte";
 
-  import { Tooltip } from "flowbite-svelte";
-
   import {
     Footer,
     FooterCopyright,
     FooterLinkGroup,
     FooterLink,
   } from "flowbite-svelte";
+
+  import Login from "./lib/comps/login.svelte";
 </script>
 
 <main class="container sm:mx-auto px-1">
@@ -45,84 +45,7 @@
   </Navbar>
 
   <div>
-    <div
-      class="w-full max-w-xs p-4 mx-auto my-5 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700"
-    >
-      <form class="space-y-6" action="#">
-        <h5 class="text-xl font-medium text-gray-900 dark:text-white">
-          Вход в личный кабинет
-        </h5>
-        <div>
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >Email</label
-          >
-          <input
-            type="email"
-            name="email"
-            id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            placeholder="name@mail.ru"
-            required
-          />
-        </div>
-        <div>
-          <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >Пароль</label
-          >
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            required
-          />
-        </div>
-        <div class="flex items-start">
-          <div class="flex items-start">
-            <div class="flex items-center h-5">
-              <input
-                id="remember"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                required
-              />
-            </div>
-            <label
-              for="remember"
-              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Remember me</label
-            >
-          </div>
-          <a
-            id="lostpwd"
-            href="#"
-            class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >Забыли пароль?</a
-          >
-          <Tooltip triggeredBy="#lostpwd" style="light"
-            >Обратитесь в деканат</Tooltip
-          >
-        </div>
-        <button
-          type="submit"
-          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >Войти</button
-        >
-        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-          Not registered? <a
-            href="#"
-            class="text-blue-700 hover:underline dark:text-blue-500"
-            >Create account</a
-          >
-        </div>
-      </form>
-    </div>
+    <Login />
   </div>
 
   <Footer
