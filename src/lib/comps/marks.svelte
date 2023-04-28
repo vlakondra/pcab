74a0fb42c331f514568c3cb6fb89e119e8f4f941
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-04-28 17:14:21 +0500

    marks-responsive

--- src/lib/comps/marks.svelte
+++ src/lib/comps/marks.svelte
@@ -41,21 +41,30 @@
 
 <div
     style="min-height: 400px;"
-    class="bg-amber-200 box-content w-auto px-8 py-8"
+    class="bg-slate-100 box-content w-auto px-8 py-8"
 >
-    <h1 class="text-center">Успеваемость</h1>
+    <h1 class="text-center">Успеваемость. {selected_sem} семестр</h1>
 
     {#await data}
         <p>Загрузка...</p>
     {:then value}
-        <div class="flex flex-row">
-            <div class="flex-grow-0 px-4 my-0">
-                <ul>
+        <div class="flex flex-col sm:flex-row">
+            <div class="flex-grow-0 px-4 my-0 w-40 dropdown dropdown-hover">
+                <label
+                    tabindex="0"
+                    class="btn btn-ghost glass w-32 btn:sm ms-0 py-1.5"
+                    >Выберите семестр</label
+                >
+                <ul
+                    style="width:165px"
+                    tabindex="0"
+                    class="dropdown-content menu w-44 p-2 shadow bg-base-100"
+                >
                     {#each semesters as item}
                         <li
                             class:active={selected_sem === item}
                             on:keydown={null}
-                            class="cursor-pointer px-1 whitespace-nowrap"
+                            class="cursor-pointer px-1 w-44 whitespace-nowrap"
                             on:click={() => (selected_sem = item)}
                         >
                             {item} семестр
@@ -63,18 +72,22 @@
                     {/each}
                 </ul>
             </div>
+
             <div class="flex-grow px-2">
                 {#each value.filter((obj) => obj.Sem == selected_sem) as item}
-                    <div class="mark-item">
+                    <div
+                        class="border-b-2 border-blue-400 grid grid-rows-[minmax(0,1fr)_auto] grid-cols-[3fr_1.5fr] items-start gap-x-1 gap-y-0 leading-none mb-1 decoration-slate-900 bg-slate-50 mark-item"
+                    >
                         <div class="subj">
                             {item.SubjSN}
                         </div>
                         <div class="fio">
                             {item.EmpFIO}
                         </div>
-                        <div class="load-mark">
-                            <span>{item.LoadKindSN}:</span>
-                            <span>{item.MarkSN}</span>
+                        <div class=" load-mark">
+                            {item.LoadKindSN}: {item.MarkSN}
+                            <!-- <span>{item.LoadKindSN}:</span>
+                            <span>{item.MarkSN}</span> -->
                         </div>
                         <div class="date-mark">
                             {new Date(item.DateMark).toLocaleDateString()}
@@ -92,17 +105,18 @@
         color: azure;
     }
     .mark-item {
-        display: grid;
+        /* display: grid; */
         /* grid-gap: 2px; */
-        row-gap: 1px;
-        column-gap: 5px;
-        grid-template-columns: 75% 25%;
-        grid-template-rows: auto auto;
+        /* row-gap: 1px;
+        column-gap: 5px; */
+        /* grid-template-columns: 75% 25%; */
+        /* grid-template-columns: 3fr 1fr; */
+        /* grid-template-rows: auto auto; */
 
-        line-height: 1;
-        margin-bottom: 5px;
-        background-color: #edf0e9;
-        color: rgb(53, 30, 30);
+        /* line-height: 1; */
+        /* margin-bottom: 5px; */
+        /* background-color: #fdfdfd;
+        color: rgb(53, 30, 30); */
     }
     .mark-item div {
         padding: 5px;
