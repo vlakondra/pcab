a093913ad4e5b64023781c69ac1e745fa30ac06d
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-05-02 18:53:14 +0500

    did marks-1

--- src/lib/comps/marks.svelte
+++ src/lib/comps/marks.svelte
@@ -6,7 +6,7 @@
     let semesters = [];
     let selected_sem = 1;
 
-    async function get_absence() {
+    async function get_marks() {
         let url = "https://api-1.ursei.su/studlk/StudMark";
         const res = await fetch(url, {
             method: "GET",
@@ -32,7 +32,21 @@
         console.log(data); // stud_marks);
     }
 
-    onMount(get_absence);
+    onMount(get_marks);
+
+    const formatMark = (mark) => {
+        if (mark.includes("зач")) {
+            return "zach";
+        } else if (mark.includes("уд")) {
+            return "udovl";
+        } else if (mark.includes("хор")) {
+            return "hor";
+        } else if (mark.includes("отл")) {
+            return "otl";
+        } else if (mark.includes("н")) {
+            return "abs";
+        }
+    };
 </script>
 
 <svelte:head>
@@ -41,30 +55,35 @@
 
 <div
     style="min-height: 400px;"
-    class="bg-slate-100 box-content w-auto px-8 py-8"
+    class="bg-slate-100 box-content w-auto px-3 py-8"
 >
-    <h1 class="text-center">Успеваемость. {selected_sem} семестр</h1>
-
     {#await data}
         <p>Загрузка...</p>
     {:then value}
-        <div class="flex flex-col sm:flex-row">
-            <div class="flex-grow-0 px-4 my-0 w-40 dropdown dropdown-hover">
-                <label
+        <h1 class="sm:block mb-5 tracking-wider text-lg text-center">
+            Успеваемость. {selected_sem} семестр
+        </h1>
+        <div class="flex flex-col m-auto max-w-3xl sm:flex-row">
+            <div
+                class="flex-grow-0 px-0 w-48 dropdown dropdown-hover sm:dropdown-open"
+            >
+                <button
                     tabindex="0"
-                    class="btn btn-ghost glass w-32 btn:sm ms-0 py-1.5"
-                    >Выберите семестр</label
+                    style=""
+                    class="btn btn-info w-full btn:sm sm:hidden z-0 mb-2 min-h-6 h-4 p-0"
                 >
+                    Выберите семестр
+                </button>
+
                 <ul
-                    style="width:165px"
                     tabindex="0"
-                    class="dropdown-content menu w-44 p-2 shadow bg-base-100"
+                    class="w-full z-0 dropdown-content menu p-2 shadow bg-cyan-200"
                 >
                     {#each semesters as item}
                         <li
                             class:active={selected_sem === item}
                             on:keydown={null}
-                            class="cursor-pointer px-1 w-44 whitespace-nowrap"
+                            class="cursor-pointer hover:border border-indigo-600 px-1 my-1 whitespace-nowrap"
                             on:click={() => (selected_sem = item)}
                         >
                             {item} семестр
@@ -73,10 +92,10 @@
                 </ul>
             </div>
 
-            <div class="flex-grow px-2">
+            <div class="flex-grow px-1">
                 {#each value.filter((obj) => obj.Sem == selected_sem) as item}
                     <div
-                        class="border-b-2 border-blue-400 grid grid-rows-[minmax(0,1fr)_auto] grid-cols-[3fr_1.5fr] items-start gap-x-1 gap-y-0 leading-none mb-1 decoration-slate-900 bg-slate-50 mark-item"
+                        class=" border-indigo-200 border-b-2 grid grid-rows-[minmax(0,1fr)_auto] grid-cols-[3fr_1.5fr] items-start gap-x-1 gap-y-0 leading-none mb-1 decoration-slate-900 bg-base-100 mark-item"
                     >
                         <div class="subj">
                             {item.SubjSN}
@@ -85,7 +104,10 @@
                             {item.EmpFIO}
                         </div>
                         <div class=" load-mark">
-                            {item.LoadKindSN}: {item.MarkSN}
+                            <span class="loadkindsn">{item.LoadKindSN}:</span>
+                            <span class={"mark " + formatMark(item.MarkSN)}>
+                                {item.MarkSN}</span
+                            >
                             <!-- <span>{item.LoadKindSN}:</span>
                             <span>{item.MarkSN}</span> -->
                         </div>
@@ -100,6 +122,17 @@
 </div>
 
 <style>
+    /* background-color: #a88c2b;
+    color: white;
+    line-height: 1em;
+    display: inline-block;
+    height: 17px;
+    width: fit-content; */
+    .loadkindsn {
+        display: inline-block;
+        width: 63px;
+    }
+
     .active {
         background-color: rgb(168, 94, 237);
         color: azure;
@@ -119,11 +152,14 @@
         color: rgb(53, 30, 30); */
     }
     .mark-item div {
-        padding: 5px;
+        padding: 2px 5px;
+        color: #7e6363;
     }
     .subj {
         grid-column: 1 / 2;
         grid-row: 1 / 2;
+        font-weight: 500;
+        color: #7e6363;
     }
     .fio {
         grid-column: 1 / 2;
@@ -136,5 +172,32 @@
     .date-mark {
         grid-column: 2 / 3;
         grid-row: 2 / 3;
+    }
+
+    .mark {
+        color: white;
+        line-height: 1rem;
+        display: inline-block;
+        height: 18px;
+        width: -moz-fit-content;
+        width: fit-content;
+        border-radius: 4px;
+        padding: 0 5px;
+        font-size: 0.75rem;
+    }
+    .zach {
+        background-color: #6a5cf1;
+    }
+    .udovl {
+        background-color: #4f4f63;
+    }
+    .otl {
+        background-color: #f29408;
+    }
+    .hor {
+        background-color: rgb(107 217 107);
+    }
+    .abs {
+        background-color: rgb(2, 2, 156);
     }
 </style>
